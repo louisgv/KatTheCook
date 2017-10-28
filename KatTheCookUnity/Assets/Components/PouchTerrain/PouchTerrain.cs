79ee7a1544ca723460c3f04995acc9a8aa688904
Joe Johans <lgvichy@gmail.com>
2017-10-28 00:17:12 -0400

    Add a simple pouch terrain

--- KatTheCookUnity/Assets/Components/PouchTerrain/PouchTerrain.cs
+++ KatTheCookUnity/Assets/Components/PouchTerrain/PouchTerrain.cs
@@ -8,25 +8,29 @@ public class PouchTerrain : MonoBehaviour
 	float[,] heightmap;
 
 	[SerializeField]
-	int resolution = 129;
+	int resolution = 100;
+
+	public Vector3 worldSize = new Vector3 (100, 100, 100);
 
 	Terrain terrain;
-
-	TerrainCollider terrainCollider;
 
 	TerrainData terrainData;
 
 	private void Awake ()
 	{
 		terrain = GetComponent <Terrain> ();
-		terrainCollider = GetComponent <TerrainCollider> ();
+
+		terrainData = terrain.terrainData;
 
 		heightmap = new float[resolution, resolution];
-		terrainData = new TerrainData ();
+
+		terrainData.heightmapResolution = resolution;
+
+		terrainData.size = worldSize;
 
 		for (int i = 0; i < resolution; i++) {
 			for (int j = 0; j < resolution; j++) {
-				heightmap [i, j] = i;
+				heightmap [i, j] = CalculateHeight (i, j, resolution);
 			}
 		}
 
@@ -34,6 +38,23 @@ public class PouchTerrain : MonoBehaviour
 
 		terrain.terrainData = terrainData;
 
-		terrainCollider.terrainData = terrainData;
+	}
+
+	private float CalculateHeight (int x, int y, int res)
+	{
+		float xCoord = ScaleRamp (x, res);
+
+		float yCoord = ScaleRamp (y, res);
+
+		float truncatedYCoord = 2f * yCoord - 1f;
+
+		float truncatedXCoord = 2f * xCoord - 1f;
+
+		return truncatedYCoord * truncatedYCoord + truncatedXCoord * truncatedXCoord;
+	}
+
+	private float ScaleRamp (int i, int res)
+	{
+		return (float)i / res;
 	}
 }
