3b95d4798c8693b5867e2c4639fb4890d6547d31
Joe Johans <lgvichy@gmail.com>
2017-10-27 21:48:56 -0400

    Adding sample terrain

---
+++ KatTheCookUnity/Assets/Components/PouchTerrain/PouchTerrain.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PouchTerrain : MonoBehaviour
+{
+
+	float[,] heightmap;
+
+	[SerializeField]
+	int resolution = 129;
+
+	Terrain terrain;
+
+	TerrainCollider terrainCollider;
+
+	TerrainData terrainData;
+
+	private void Awake ()
+	{
+		terrain = GetComponent <Terrain> ();
+		terrainCollider = GetComponent <TerrainCollider> ();
+
+		heightmap = new float[resolution, resolution];
+		terrainData = new TerrainData ();
+
+		for (int i = 0; i < resolution; i++) {
+			for (int j = 0; j < resolution; j++) {
+				heightmap [i, j] = i;
+			}
+		}
+
+		terrainData.SetHeights (0, 0, heightmap);
+
+		terrain.terrainData = terrainData;
+
+		terrainCollider.terrainData = terrainData;
+	}
+}
